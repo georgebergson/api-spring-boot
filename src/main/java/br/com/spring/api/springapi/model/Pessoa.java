47a30dcb3c196993861c7e4807d34eeed41e8d8f
george.castilho <george.castilho@apexbrasil.com.br>
2023-02-17 12:06:34 -0400

    Conexao Mysql

--- src/main/java/br/com/spring/api/springapi/model/Pessoa.java
+++ src/main/java/br/com/spring/api/springapi/model/Pessoa.java
@@ -1,11 +1,27 @@
 package br.com.spring.api.springapi.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "pessoas")
 public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int codigo;
     private String nome;
     private int idade;
 
     // getters e setters
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
     public void setNome(String nome) {
         this.nome = nome;
     }
